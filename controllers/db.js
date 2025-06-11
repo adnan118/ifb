2cfee35f4a8960911c70c20cc11388401512297c
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-06-11 05:17:51 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -9,7 +9,7 @@ const mysql = require("mysql2/promise");
   database: "railway",
 };*/
 const dbConfig = {
-  host: "dreamtsv.com",
+  host: "pos-mng",
   user: "u643198768_project_ifb",
   password: "projectIfb.118259",
   database: "u643198768_project_ifb",
