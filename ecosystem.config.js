e2e47a1ed04c5b11463b5b0e8ee85c0a34e215f2
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-06-12 03:13:28 -0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "ifb",
+      script: "server.js",
+      instances: 1,  
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+        DB_HOST: "localhost",
+        DB_USER: "root",
+        DB_PASSWORD: "",
+        DB_NAME: "test",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3118,
+        DB_HOST: "dreamtsv.com",
+        DB_USER: "u643198768_project_ifb",
+        DB_PASSWORD: "CHKX2JYOL7^a",
+        DB_NAME: "u643198768_project_ifb",
+      },
+    },
+  ],
+};
