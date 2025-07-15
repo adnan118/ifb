3b107e093d299d4f847f395a60acedeefbbdcbc8
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-07-15 05:02:12 -0700

    Update FgpasswordRout.js

--- routes/authRoutes/FgpasswordRout.js
+++ routes/authRoutes/FgpasswordRout.js
@@ -1,9 +1,13 @@
 // routes/routes.js
 const express = require("express");
 const { FgPassword } = require("../../query/auth/fgpassword");
+const { updatePassword } = require("../../query/auth/updatePassword");
 
 const router = express.Router();
 
-router.post("/fgpassword", FgPassword);
+router.post("/fgpassword", FgPassword);     
+router.post("/updatePassword", updatePassword);     
 
 module.exports = router;
+
+  
