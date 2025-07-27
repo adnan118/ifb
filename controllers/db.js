ea4daa9cbc9284e64baa08de1652a380aed4ed73
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-07-27 02:00:09 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2/promise");
 
 const dbConfig = {
-  host: process.env.DB_HOST,
+/*  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+*/
+          host: 3120,
+  user: "root",
+  password: "ifb118",
+  database: "ib",
+ 
 };
 
 async function getConnection() {
