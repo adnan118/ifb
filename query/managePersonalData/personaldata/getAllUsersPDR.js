d867f5e8fbae90bbb2e6f1ffd72124b7ca9cf4d5
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-08-05 03:13:58 -0700

    Update getAllUsersPDR.js

--- query/managePersonalData/personaldata/getAllUsersPDR.js
+++ query/managePersonalData/personaldata/getAllUsersPDR.js
@@ -10,8 +10,8 @@ async function getAllUsersPDR(req, res) {
     // Get all users' personal data register with offers_id and users_id only
     const connection = await getConnection();
     
-    const query = `
-      SELECT personalData_users_id, personalData_offers_id, personalData_goal_id 
+      const query = `
+      SELECT personalData_users_id, personalData_offers_id, personalData_goal_id, personalData_dietType_id 
       FROM personaldataregister 
       WHERE personalData_offers_id IS NOT NULL AND personalData_users_id IS NOT NULL
     `;
