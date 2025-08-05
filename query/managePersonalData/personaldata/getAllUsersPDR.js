440613c698a514bbee790a1d60bcf655993eed1c
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-08-05 01:58:25 -0700

    Create getAllUsersPDR.js

---
+++ query/managePersonalData/personaldata/getAllUsersPDR.js
@@ -0,0 +1,33 @@
+const { getData } = require("../../../controllers/functions");
+
+async function getAllUsersPDR(req, res) {
+  try {
+    // Get all users' personal data register with offers_id and users_id
+    const result = await getData(
+      "personaldataregister",
+      "personalData_offers_id IS NOT NULL AND personalData_users_id IS NOT NULL",
+      [], // No parameters needed since we're getting all users
+    );
+
+    if (result.status === "success") {
+      res.status(200).json({
+        status: "success",
+        data: result.data,
+      });
+    } else {
+      res.status(500).json({
+        status: "failure",
+        message: "Failed to get data.",
+      });
+    }
+  } catch (error) {
+    console.error("Error getting data: ", error);
+    res.status(500).json({
+      status: "failure",
+      message: "There is a problem getting data",
+    });
+  }
+}
+
+// تصدير الدالة
+module.exports = { getAllUsersPDR }; 
