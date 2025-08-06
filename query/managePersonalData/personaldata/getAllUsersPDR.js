c604ce483f3f3c74983008d7fb32b1fb7d489f91
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-08-06 03:19:14 -0700

    Update getAllUsersPDR.js

--- query/managePersonalData/personaldata/getAllUsersPDR.js
+++ query/managePersonalData/personaldata/getAllUsersPDR.js
@@ -13,7 +13,7 @@ async function getAllUsersPDR(req, res) {
       const query = `
       SELECT personalData_users_id, personalData_offers_id, personalData_goal_id, personalData_dietType_id ,personalData_gender_id,
       FROM personaldataregister 
-      WHERE personalData_offers_id IS NOT NULL AND personalData_users_id IS NOT NULL
+      WHERE personalData_offers_id IS NOT NULL AND personalData_users_id IS NOT NULL  AND personalData_gender_id IS NOT NULL
     `;
     
     const [results] = await connection.execute(query);
