55cb07751dfae72500b7d5dc5668fe333c1f305a
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-08-13 03:38:08 -0700

    Update personalDataRegisterRout.js

--- routes/personaldata/personalDataRegisterRout.js
+++ routes/personaldata/personalDataRegisterRout.js
@@ -13,9 +13,8 @@ const {
 } = require("../../query/managePersonalData/personaldata/getPDR");
 
 const {
-  getAllUsersPDR,
-} = require("../../query/managePersonalData/personaldata/getAllUsersPDR");
-
+  updatePaymentStatus,
+} = require("../../query/managePersonalData/personaldata/updatePaymentStatus");
 
 const router = express.Router();
 
@@ -26,9 +25,9 @@ const router = express.Router();
 router.post("/insertPersonalDataRegister", insertPersonalDataRegister);
 router.post("/updatePDR", updatePDR);
 router.post("/getPDR", getPDR);
-router.post("/getAllUsersPDR", getAllUsersPDR);
+
+// مسار تحديث حالة الدفع
+router.post("/updatePaymentStatus", updatePaymentStatus);
 
 ;
 module.exports = router;
-
-
