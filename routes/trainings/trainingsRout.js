7f0096f43a6e18835e55d596a607494aca1bdaed
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-08-21 04:47:38 -0700

    Update trainingsRout.js

--- routes/trainings/trainingsRout.js
+++ routes/trainings/trainingsRout.js
@@ -17,13 +17,13 @@ const express = require('express');
     updateAvailabilityTraining,
   } = require("../../query/trainings/updateAvailabilityTraining");
  const { getTrainingsByActivityId } = require("../../query/trainings/getTrainingsByActivityId");
-/*
+
  // استيراد وظائف user_trainings
  const { insertDataUserTraining } = require("../../query/user_trainings/insertDataUserTraining");
  const { updateDataUserTraining } = require("../../query/user_trainings/updateDataUserTraining");
  const { deleteDataUserTraining } = require("../../query/user_trainings/deleteDataUserTraining");
  const { getDataUserTrainingsByUserId } = require("../../query/user_trainings/getDataUserTraining");
- */
+ 
  const router = express.Router();
  
  router.post("/insertDataTraining", uploadImages, insertDataTraining);
@@ -34,16 +34,17 @@ const express = require('express');
  router.post("/getDataTraining", getDataTraining);
  router.post("/deleteDataTraining", deleteDataTraining, deleteImages);
   router.post("/updateAvailabilityTraining", updateAvailabilityTraining);
-/*
+
  // روابط user_trainings
 router.post("/insertUserTraining", insertDataUserTraining);
 router.post("/updateUserTraining", updateDataUserTraining);
 router.post("/deleteUserTraining", deleteDataUserTraining);
 router.post("/getUserTrainings", getDataUserTrainingsByUserId);
-*/
+
 
  
 module.exports = router; 
+
 
 
 
