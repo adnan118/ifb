a784c17990afdff62cc47ef47eb60c23f67503fb
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-09-13 03:29:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -150,25 +150,33 @@ const FinancialRout = require("./routes/Financial/FinancialRout");
 
 app.use(express.json());
  // خدمة الملفات الثابتة
-app.use(express.static(reactBuildPath));
-app.use('/uploadvideo', express.static(reactBuildPath2)); // ضع prefix لتفادي التعارض
+  app.use(express.json());
+// 1) مسار البناء الخاص بتطبيق React
+const reactBuildPath = path.join(__dirname, 'frontend_build');
 
-// مسارات root المخصصة
+// 2) خدمة الملفات الثابتة لـ React
+app.use(express.static(reactBuildPath));
+
+// 1) مسار البناء الخاص بتطبيق React
+const reactBuildPath2 = path.join(__dirname, 'uploadvideo');
+
+// 2) خدمة الملفات الثابتة لـ React
+app.use(express.static(reactBuildPath2));
+
+// 3) توجيه الجذر إلى React
 app.get('/', (req, res) => {
   res.sendFile(path.join(reactBuildPath, 'index.html'));
 });
-
+// 3) توجيه الجذر إلى React
 app.get('/uploadvideo', (req, res) => {
   res.sendFile(path.join(reactBuildPath2, 'index.html'));
 });
 
-// لتعامل مع كل المسارات الأخرى: 
-// إذا كان الطلب يبدأ بـ /uploadvideo نعيد index داخل uploadvideo، وإلا نعيد index لواجهة الفرونت
+// 4) دعم المسارات الفرعية لـ React Router
 app.get('*', (req, res) => {
-  if (req.path.startsWith('/uploadvideo')) {
-    return res.sendFile(path.join(reactBuildPath2, 'index.html'));
-  }
-  return res.sendFile(path.join(reactBuildPath, 'index.html'));
+  // إذا كان الملف المطلوب موجوداً كـ static فسيتم خدمته قبل هذا المسار
+  // وإلا فسنعيد index.html ليعالج العميل من جانب React
+  res.sendFile(path.join(reactBuildPath, 'index.html'));
 });
 /*
 app.get("/", (req, res) => {
@@ -354,6 +362,7 @@ app.use("/api84818datafinancial", FinancialRout);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on Port:${PORT}`);
 });
+
 
 
 
