978a501b6be6f8c2f91a31382de04dcc11673053
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-09-18 03:36:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const path = require("path");
 // Serve static files from the 'query' directory
 app.use('/query', express.static(path.join(__dirname, 'query'))); // Custom route for user images with fallback to default
 app.get('/query/auth/userImages/images/:filename', (req, res) => { const filePath = path.join(__dirname, 'query/auth/userImages/images', req.params.filename);
-  
+app.use('/api/query/food/foodImages/images', express.static(path.join(__dirname, 'query', 'food', 'foodImages', 'images')));
+
   if (fs.existsSync(filePath)) {
     res.sendFile(filePath);
   } else if (req.params.filename === 'logo.png') {
@@ -359,6 +360,7 @@ app.use("/api84818datafinancial", FinancialRout);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on Port:${PORT}`);
 });
+
 
 
 
