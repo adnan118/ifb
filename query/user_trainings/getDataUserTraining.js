693c45e733b5243f34bcaef8c52d919007318d5d
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2025-12-09 05:23:13 -0800

    Update getDataUserTraining.js

--- query/user_trainings/getDataUserTraining.js
+++ query/user_trainings/getDataUserTraining.js
@@ -1,4 +1,4 @@
-/*
+
 
 const { getData ,getAllData} = require("../../controllers/functions");
 
@@ -42,63 +42,3 @@ module.exports = {
   getDataUserTrainingsByUserId,
 
 };
-*/
-const { getData ,getAllData} = require("../../controllers/functions");
-
-// جلب تدريبات مستخدم معين
-const getDataUserTrainingsByUserId = async (req, res) => {
-  try {
-    const { user_id } = req.body;
-
-    if (!user_id) {
-      return res.status(400).json({
-        status: "failure",
-        message: "Missing required parameter: user_id",
-      });
-    }
-
-    const result = await getAllData("user_trainings", "user_id = ?", [user_id]);
-
-    if (result.status === "success") {
-      // Process the equipment weights data before sending response
-      const processedData = result.data.map(training => {
-        if (training.equipment_weights) {
-          try {
-            // Try to parse as JSON
-            training.equipment_weights_parsed = JSON.parse(training.equipment_weights);
-          } catch (e) {
-            // If not valid JSON, keep as string
-            training.equipment_weights_parsed = training.equipment_weights;
-          }
-        }
-        return training;
-      });
-
-      res.status(200).json({
-        status: "success",
-        message: "User trainings fetched successfully",
-        data: processedData,
-      });
-    } else {
-      res.status(500).json({
-        status: "failure",
-        message: result.message || "Error fetching user trainings",
-      });
-    }
-  } catch (error) {
-    console.error("Error in getData user trainings by user_id:", error);
-    res.status(500).json({
-      status: "failure",
-      message: "Internal server error",
-      error: error.message,
-    });
-  }
-};
-
-module.exports = {
-  getDataUserTrainingsByUserId,
-};
-
-
-
-
