dc733417a1b4f5b54a45e365515037f8e4296e7b
Adnan Ahmad Al Barakat <73432559+adnan118@users.noreply.github.com>
2026-04-06 04:34:27 -0700

    Update LoginUserRout.js

--- routes/authRoutes/LoginUserRout.js
+++ routes/authRoutes/LoginUserRout.js
@@ -17,3 +17,20 @@ router.post("/loginAdmin", LoginAdmin);
 
 
 module.exports = router;
+
+/*
+// routes/routes.js
+const express = require("express"); 
+const { getUserData } = require("../../query/auth/getUserData");
+const { LoginUser, LoginAdmin } = require("../../query/auth/login");
+
+const router = express.Router();
+
+ 
+router.post("/login", LoginUser);
+router.post("/getUserData", getUserData);
+router.post("/loginAdmin", LoginAdmin);
+
+
+module.exports = router;
+*/
